08e9dc5d74d9dc9f8a0f1de72a5af728787034e5
Blake Wood <blakeandrewwood@gmail.com>
2017-12-18 01:20:38 -0800

    progress on news and topics

--- server/lib/server_web/controllers/page_controller.ex
+++ server/lib/server_web/controllers/page_controller.ex
@@ -7,9 +7,33 @@ defmodule ServerWeb.PageController do
     comments = Content.list_top_level_comments()
     num_comments = Content.count_comments(comments)
 
+    topics = ["Topic A", "Topic B", "Topic C"]
+    news = [
+      %{
+        :title => "Title A",
+        :excerpt => "Excerpt A",
+        :link => "http://google.com",
+        :image => "http://materializecss.com/images/sample-1.jpg"
+      },
+      %{
+        :title => "Title B",
+        :excerpt => "Excerpt B",
+        :link => "http://google.com",
+        :image => "http://materializecss.com/images/sample-1.jpg"
+      },
+      %{
+        :title => "Title C",
+        :excerpt => "Excerpt C",
+        :link => "http://google.com",
+        :image => "http://materializecss.com/images/sample-1.jpg"
+      }
+    ]
+
     conn
     |> assign(:comments, comments)
     |> assign(:num_comments, num_comments)
+    |> assign(:news, news)
+    |> assign(:topics, topics)
     |> render("index.html")
   end
 end
